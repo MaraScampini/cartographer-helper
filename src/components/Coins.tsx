a25648f491718531d8d8a2c46b8021627947333d
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-11-19 21:37:35 +0100

    UI fix

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -9,27 +9,27 @@ const Coins = () => {
   const [isClicking, setIsClicking] = useState(false);
 
   const handleClick = (index: number): void => {
-    if (isClicking) return; // Ignore if already clicking
+    if (isClicking) return;
     setIsClicking(true);
-    
+
     setSelectedIndexes((prevState) => ({
       ...prevState,
       [index]: !prevState[index]
     }));
-  
+
     setTimeout(() => {
-      setIsClicking(false); // Reset after a short delay
-    }, 300); // Adjust delay as needed
+      setIsClicking(false);
+    }, 300);
   };
 
   return (
     <div className="mt-5 flex justify-center items-center space-x-2 p-3 bg-ruin rounded-lg">
       {[...Array(14)].map((_, index) => (
         <div onClick={() => handleClick(index)} onTouchEnd={() => handleClick(index)} key={index}
-          className={`w-6 h-6 bg-yellow-200 rounded-full flex items-center justify-center shadow-md hover:cursor-pointer ${selectedIndexes[index] ? 'bg-purple-500 shadow-[inset_1px_1px_1px_1px_rgba(0,_0,_0,_0.3)]' : ''
+          className={`w-6 h-6 bg-yellow-200 rounded-full flex items-center justify-center shadow-md hover:cursor-pointer ${selectedIndexes[index] ? 'shadow-[inset_1px_1px_1px_1px_rgba(0,_0,_0,_0.3)]' : ''
             }`}
         >
-          
+
         </div>
       ))}
     </div>
