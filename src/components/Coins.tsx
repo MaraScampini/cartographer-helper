11a232202e453eed1cd085271bce3153381ca6f1
Mara Scampini <109754827+MaraScampini@users.noreply.github.com>
2024-11-20 08:45:59 +0100

    test

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -26,7 +26,7 @@ const Coins = () => {
     <div className="mt-5 flex justify-center items-center space-x-2 p-3 bg-ruin rounded-lg">
       {[...Array(14)].map((_, index) => (
         <div onClick={() => handleClick(index)} onTouchEnd={() => handleClick(index)} key={index}
-          className={`w-6 h-6 bg-yellow-200 rounded-full flex items-center justify-center shadow-md hover:cursor-pointer ${selectedIndexes[index] ? 'shadow-[inset_1px_1px_1px_1px_rgba(0,_0,_0,_0.3)]' : ''
+          className={`w-6 h-6 bg-yellow-200 rounded-full flex items-center justify-center shadow-md hover:cursor-pointer ${selectedIndexes[index] ? 'bg-purple-500 shadow-[inset_1px_1px_1px_1px_rgba(0,_0,_0,_0.3)]' : ''
             }`}
         >
 
